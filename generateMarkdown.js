e47c07794c5240202cb52ae0790117d2dd17ea3e
bechedley <109792989+bechedley@users.noreply.github.com>
2022-10-08 03:50:03 +1100

    Add package files

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -4,11 +4,30 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    
+    if licence === "MIT" {
+        `Licensed under the standard MIT license. Please refer to the license in the repo for more information.`
+    }
+    if licence === "Apache 2.0" {
+        `Licensed under the standard Apache 2.0 license. Please refer to the license in the repo for more information.`
+    }
+    if licence === "GPLv3" {
+        `Licensed under the standard GNU GPLv3 license. Please refer to the license in the repo for more information.`
+    }
+    if licence === "BSD 3" {
+        `Licensed under the standard BSD 3 license. Please refer to the license in the repo for more information.`
+    }
+    if licence === "None" {
+        ``
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
